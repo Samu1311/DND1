1edb138161c68dfe13799ed73cd8804f6b51432b
Samuele <samuele13112002@gmail.com>
2024-10-29 14:14:39 +0100

    Update assembly version in WebApp and WebServiceAPI projects

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63b69dce79986ca97f9991f6efe8e23769625fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf6648a50d749eef552d52e04b165c40e92e2d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
