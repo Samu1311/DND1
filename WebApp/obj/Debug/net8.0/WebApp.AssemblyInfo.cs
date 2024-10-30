0c3a4a13af4887ebaa7201a1f852d8fe7ac86366
Samuele <samuele13112002@gmail.com>
2024-10-30 16:55:38 +0100

    Remove Counter and Weather pages; update assembly info and static web assets

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7219c9441383a6c7fc2535a883fd258bb97134c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a6c839ca5ca6f3cd52c5b149603cd8f3d24e886")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
