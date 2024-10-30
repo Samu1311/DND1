4a6c839ca5ca6f3cd52c5b149603cd8f3d24e886
Samuele <samuele13112002@gmail.com>
2024-10-30 16:17:14 +0100

    Enhance HomePage layout and styles; add testimonials and call-to-action sections; update NavMenu and MainLayout styles

--- WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
+++ WebApp/obj/Debug/net8.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edb138161c68dfe13799ed73cd8804f6b51432b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7219c9441383a6c7fc2535a883fd258bb97134c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
