e7219c9441383a6c7fc2535a883fd258bb97134c
Samuele <samuele13112002@gmail.com>
2024-10-30 15:24:25 +0100

    Enhance navbar layout and styles; update assembly info for WebApp and WebServiceAPI

--- WebServiceAPI/obj/Debug/net8.0/WebServiceAPI.AssemblyInfo.cs
+++ WebServiceAPI/obj/Debug/net8.0/WebServiceAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebServiceAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf6648a50d749eef552d52e04b165c40e92e2d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1edb138161c68dfe13799ed73cd8804f6b51432b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebServiceAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebServiceAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
